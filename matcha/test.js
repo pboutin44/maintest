d58b4b6926b7763d46150a4b5fffbe78002d225d
pierro <pboutin@student.42.fr>
2017-02-08 20:01:32 +0100

    matcha

---
+++ matcha/test.js
@@ -0,0 +1,29 @@
+var express = require('express');
+
+var app = express();
+app.use(express.static('assets'));
+
+app.get('/', function(req, res) {
+  res.render('page.ejs');
+});
+app.get('/connexion', function(req, res) {
+  res.render('login.ejs');
+});
+app.get('/register', function(req, res) {
+  res.render('register.ejs');
+});
+app.get('/sous-sol', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Vous êtes dans la cave à vins, ces bouteilles sont à moi !');
+});
+
+app.get('/etage/1/chambre', function(req, res) {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hé ho, c\'est privé ici !');
+});
+app.get('/compter', function(req, res) {
+    res.render('page.ejs');
+});
+
+
+app.listen(8080);
