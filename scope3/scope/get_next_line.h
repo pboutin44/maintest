16b43b855990c9823c0f7fc13cace8d10a3c0576
pierro <pboutin@student.42.fr>
2017-06-29 19:06:12 +0200

    todololo

---
+++ scope3/scope/get_next_line.h
@@ -0,0 +1,10 @@
+
+#ifndef __GET_NEXT_LINE_H__
+# define __GET_NEXT_LINE_H__
+
+# define READ_BUFF 500
+# define RETURN_BUFF 500
+
+char		*get_next_line(const int fd);
+
+#endif /* !__GET_NEXT_LINE_H__ */
