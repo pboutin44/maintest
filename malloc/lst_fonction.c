034806884db94df8b96bd5adebd9e795e0af3a23
pierro <pboutin@student.42.fr>
2017-09-16 21:43:53 +0200

    dw

---
+++ malloc/lst_fonction.c
@@ -0,0 +1,75 @@
+#include	"malloc.h"
+
+void	*ft_memcpy(void	*dst, const void *src, size_t n)
+{
+    size_t          i;
+    const char      *src1;
+    char            *dst1;
+
+    i = 0;
+    src1 = src;
+    dst1 = dst;
+    while (i < n)
+    {
+        dst1[i] = src1[i];
+        i++;
+    }
+    return (dst1);
+}
+
+ t_list  *ft_lstnew(void const *content, size_t content_size)
+{
+    t_list *new;
+
+    if (!(new = (t_list *)mmap(0, sizeof(t_list), PROT_READ | PROT_WRITE, MAP_ANON | MAP_PRIVATE, -1, 0))) //normally PSIZE
+        return (NULL);
+    if (content == NULL)
+    {
+        new->content = NULL;
+        new->content_size = 0;
+    }
+    else
+    {
+        new->content = (char *)content;
+        new->content_size = content_size;
+    }
+    new->next = NULL;
+    return (new);
+}
+
+
+ t_list    *browse_lst(t_list *lst)
+ {
+     if (lst)
+     {
+         while (lst->next)
+         {
+             lst = lst->next;
+         }
+         return(lst);
+     }
+     return(NULL);
+ }
+
+
+t_list  *ft_lstiter(t_list *lst, int (f)(t_list *elem))
+{
+	t_list  *tmp;
+
+	if(lst && f)
+	{
+		tmp = lst;
+		while(tmp)
+		{
+			if(f(tmp) == 1)
+			{
+				return(tmp);
+			}
+			tmp = tmp->next;
+		}
+	}
+	printf("COUCOU JE RETOURNE NULL\n");
+	return(NULL);
+}
+
+
--- malloc/malloc.h
+++ malloc/malloc.h
@@ -9,14 +9,14 @@
 # define FALSE '0'
 typedef char			t_bool;
 
-typedef struct			s_tiny
+/*typedef struct			s_tiny
 {
 	void				*addr;
 	size_t				size;
 //	t_bool				tab[128];
 	struct s_tiny		*next;
 }						t_tiny;
-
+*/
 
 typedef struct      s_list
 {
@@ -26,7 +26,7 @@ typedef struct      s_list
 }                   t_list;
 
 
-typedef struct			s_small
+/*typedef struct			s_small
 {
 	void				*addr;
 	size_t				size;
@@ -39,18 +39,36 @@ typedef	struct			s_large
 	void				*addr;
 	size_t				size;
 	struct	s_large		*next;
-}						t_large;
+}						t_large;*/
 
 typedef struct			s_malloc
 {
 	t_list				*tiny;
-	t_small				*small;
-	char				*large;
+	t_list				*small;
+	t_list				*large;
 }						t_malloc;
-	void	*choose_memory_adress(size_t size);
 
-extern	t_malloc		zone1;
+	void	*ft_malloc(size_t size);
+	void	*ft_memcpy(void	*dst, const void	*src, size_t n);
+	t_list	*ft_lstnew(void const	*content, size_t	content_size);
+	t_list	*browse_lst(t_list	*lst);
+	t_list	*ft_lstiter(t_list	*lst, int	(f)(t_list	*elem));
+	void	*size_tiny(size_t	size);
+	void	*tiny_exist(size_t size);
+	void	*another_tiny(size_t size);
+	void	*first_tiny(size_t size);
+	int		one_place_atleast(t_list	*elem);
+	int		set_tinyheader(t_list	*lst);
+	int		set_smallheader(t_list	*lst);
+	void	*another_small(size_t	size);
+	void	*first_small(size_t	size);
+	void	*small_exist(size_t	size);
+	void	*size_small(size_t	size);
+	int		one_place_atleastsmall(t_list	*elem);
+	void	*large_size(size_t	size);
+	void	ft_free(void	*ptr);
+
+
 t_malloc				zone;
-extern	int				test;
-int		test2;
+
 #endif
